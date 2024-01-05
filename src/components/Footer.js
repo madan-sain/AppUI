ca5cbb3f7a12d17bd0e361a65a4eca869d1c5955
madan <btp.madansain@gmail.com>
2024-01-05 22:19:42 +0530

    add chnegs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-black/80 h-[50vh] flex justify-center items-center'>
+      <div className='w-full flex justify-center'>
+
+      <div className='text-center md:mx-w-4xl'>
+          <h1 className='text-center text-white text-2xl md:text:4xl'><span className='text-red-400' >App</span>Lab</h1>
+          <div className='grid grid-cols-2 md:grid-cols-5 text-white mt-5'>
+<div className='text-lg'>Home</div>
+<div className='text-lg'>Key Features</div>
+<div className='text-lg'>Pricing</div>
+<div className='text-lg'>Testiminial</div>
+<div className='text-lg'>FAQ</div>
+          </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
